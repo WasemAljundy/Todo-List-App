8a2be48102abc183359e715e65c750ac564d20b2
WasemAljundy <wasemaljundy22@gmail.com>
2023-08-22 12:37:21 +0300

    project structure prepared

---
+++ android/app/src/main/java/com/wasem/getx_todo_list/MainActivity.java
@@ -0,0 +1,6 @@
+package com.wasem.getx_todo_list;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:getx_todo_list/app/data/services/storage/services.dart';
+import 'package:getx_todo_list/app/modules/home/view.dart';
+
+void main() async {
+  await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Todo List using GetX',
+      home: HomePage(),
+    );
+  }
+}
+
